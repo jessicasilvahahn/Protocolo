e94b2447f50d0418bdb1558a0cf20018e71ae06d
jessica.hahn <jessica.hahn.silva@gmail.com>
2018-08-05 14:34:49 -0300

    Transferencia de arquivo via socketUdp

--- myProtocol/ChannelUDPClient.py
+++ myProtocol/ChannelUDPClient.py
@@ -4,10 +4,18 @@ HOST = ''  # Endereco IP do Servidor
 PORT = 3400            # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
-print ('Para sair use CTRL+X\n')
-msg = input()
-while  msg != '\x18':
-    udp.sendto(msg.encode(), dest)
-    msg = input()
-    serverMsg,addrServer = udp.recvfrom(512)
+msg = "/home/jessica/Documentos/teste/teste.txt"
+fileNameClient = "/home/jessica/Documentos/teste2.txt"
+udp.sendto(msg.encode(), dest)
+serverMsg,addrServer = udp.recvfrom(512)
+fileClient = open(fileNameClient, 'w')
+while(serverMsg.decode()!="finish"):
     print("Recebido de:",serverMsg,addrServer)
+    print("Sending to File\n")
+    fileClient.write(serverMsg.decode('utf-8'))
+    serverMsg, addrServer = udp.recvfrom(512)
+fileClient.close()
+print("Arquivo Transferido\n")
+udp.close()
+
+
--- myProtocol/ChannelUDPServer.py
+++ myProtocol/ChannelUDPServer.py
@@ -1,12 +1,20 @@
 import socket
+import sys
 HOST = ''              # Endereco IP do Servidor
 PORT = 3400            # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
 udp.bind(orig)
 while True:
-    msg, cliente = udp.recvfrom(512)
-    print(cliente, msg)
-    print("Mensagem server\n")
-    msgServer = input()
-    udp.sendto(msgServer.encode(),(cliente[0],cliente[1]))
+    fileName, cliente = udp.recvfrom(512)
+    print(cliente, fileName)
+    fileServer = open(fileName,'r')
+    data = fileServer.read(512)
+    while(data):
+        print(data,"\n")
+        if(udp.sendto(data.encode(),(cliente[0],cliente[1]))):
+                print("Sending\n")
+                data = fileServer.read(512)
+    print("Enviando finish")
+    udp.sendto("finish".encode(), (cliente[0], cliente[1]))
+    fileServer.close()
